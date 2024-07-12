cf357a0bda1b5fcdfd1ce1dd495a45c83c754079
Crisluna13 De Luna <2122000512@utna.edu.mx>
2024-07-12 13:10:50 +0000

    Diseno del login

--- src/frontend/src/components/Login.jsx
+++ src/frontend/src/components/Login.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { AuthClient } from '@dfinity/auth-client';
+import logo from '../../public/log.png';
+import logoo from '../../public/ic.png';
+import nFondo from '../../public/logoo.png';
+import utna from '../../public/utnaa.png';
+import utma from '../../public/utmaa.png';
+import icphub from '../../public/icphub.png';
+import z3 from '../../public/z3.jpeg';
+import df from '../../public/df.png';
+import '../components/style.css';
 
 const Login = ({ onLogin }) => {
   const [authClient, setAuthClient] = useState(null);
@@ -25,17 +34,32 @@ const Login = ({ onLogin }) => {
   };
 
   return (
-    <div style={{ backgroundColor: '#A6E1FA', padding: '10px 0', textAlign: 'center', color: '#ffffff' }}>
-      <h1>Mi Sitio Web</h1>
-      <button className="btn btn-primary" onClick={handleLogin}>
+    <div className="body-container">
+      <img src={logo} alt="Logo" className="logo" />
+
+      <div className="background-image-container">
+        <div className="heading">
+        <img src={nFondo} alt="Logo Overlay" className="logo-overlay"  />
+
+
+        </div>
+      </div>
+      <br />
+      <br />
+
+      <button className="customb" onClick={handleLogin}>
         Login with Internet Identity
       </button>
+      <div className="footer">
+      <img className ="logoo" src={logoo} alt="Logo" />
+      <img src={utna} alt="Logo" className="utna" />
+  <img src={utma} alt="Logo" className="utma" />
+  <img src={z3} alt="Logo" className="z3" />
+  <img src={icphub} alt="Logo" className="icph" />
+  <img src={df} alt="Logo" className="df" />
+</div>
     </div>
   );
 };
 
 export default Login;
-
-
-
-
