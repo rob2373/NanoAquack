d48ea97e9978eb48a90521e2d14a3dc14d80694b
Crisluna13 De Luna <2122000512@utna.edu.mx>
2024-07-13 11:33:23 +0000

    Ultimos detalles del citas.jsx

--- src/frontend/src/components/citas.jsx
+++ src/frontend/src/components/citas.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
 import '../components/citas.css'; // Importa el archivo de estilos CSS
-import logo from '../../public/log.png';
-import logoo from '../../public/ic.png';
-import nFondo from '../../public/logoo.png';
-import utna from '../../public/utnaa.png';
-import utma from '../../public/utmaa.png';
-import icphub from '../../public/icphub.png';
-import z3 from '../../public/z3.jpeg';
-import df from '../../public/df.png';
-import citas from '../public/ci.png';
+import logo from '/log.png';
+import logoo from '/ic.png';
+import nFondo from '/logoo.png';
+import utna from '/utnaa.png';
+import utma from '/utmaa.png';
+import icphub from '/icphub.png';
+import z3 from '/z3.jpeg';
+import df from '/df.png';
+import citas from '/ci.png';
+import out from '/out.png';
 
 const RegisterForm = () => {
   const [formData, setFormData] = useState({
@@ -39,108 +40,107 @@ const RegisterForm = () => {
     }
   };
 
+  const handleLogout = () => {
+    // Aquí puedes añadir la lógica para el logout de Internet Identity
+    // Por ejemplo, limpiar tokens de sesión, redirigir a la página de login, etc.
+    console.log("Logout de Internet Identity");
+    // Redirigir a la página de login o a la página de inicio
+    window.location.href = 'Login'; // Reemplaza con la ruta correcta a tu página de login
+  };
+
   // Redirigir si el formulario ha sido enviado correctamente
   if (formSubmitted) {
-    return <Redirect to="" />; // Reemplaza "/ruta-de-tu-pagina" con la ruta real a donde deseas redirigir
+    return <Redirect to="/Login" />; // Reemplaza "/ruta-de-tu-pagina" con la ruta real a donde deseas redirigir
   }
 
   return (
     <body>  
-    <div className="body-container" style={{height: '50px'}}>
-    <img src={citas} alt="Citas" className="citas" />
-      <img src={logo} alt="Logo" className="logo" />
+      <div className="body-container" style={{ height: '50px' }}>
+        <img 
+          src={out} 
+          alt="out" 
+          className="out" 
+          onClick={handleLogout} // Llama a la función de logout al hacer clic
+        />
+        <img src={citas} alt="citas" className="citas" />
+        <img src={logo} alt="Logo" className="logo" />
       </div>
-    <Container className="register-form-container">
-      <h2 className="form-title">Registro de Citas</h2>
-      <Form onSubmit={handleSubmit} className="form">
-        <Form.Group controlId="dateC">
-        <br />
-      
-          <Form.Label className="form-label">Fecha de cita</Form.Label>
-          <br />
-          <Form.Control
-            type="text"
-            placeholder="Ingrese la fecha en que desea la cita"
-            name="dateC"
-            value={formData.companyName}
-            onChange={handleChange}
-            className="form-input"
-            required
-          />
+      <Container className="citas-form-container">
+        <h2 className="form-title">Registro de Citas</h2>
+        <Form onSubmit={handleSubmit} className="form">
+          <Form.Group controlId="dateC">
             <br />
-           
-        </Form.Group>
+            <Form.Label className="form-label">Fecha de cita</Form.Label>
+            <br />
+            <Form.Control
+              type="text"
+              placeholder="Ingrese la fecha en que desea la cita"
+              name="dateC"
+              value={formData.dateC}
+              onChange={handleChange}
+              className="form-input"
+              required
+            />
+            <br />
+          </Form.Group>
       
-        <Form.Group controlId="Litros">
-          <Form.Label className="form-label">Litros Aproximados</Form.Label>
-          <br />
-          <Form.Control
-            type="string"
-            placeholder="Ingrese cuantos litros desea"
-            name="Litros"
-            value={formData.email}
-            onChange={handleChange}
-            className="form-input"
-            required
-          />
-        </Form.Group>
+          <Form.Group controlId="Litros">
+            <Form.Label className="form-label">Litros Aproximados</Form.Label>
+            <br />
+            <Form.Control
+              type="string"
+              placeholder="Ingrese cuantos litros desea"
+              name="Litros"
+              value={formData.Litros}
+              onChange={handleChange}
+              className="form-input"
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId="all">
-             <br />
-          <Form.Label className="form-label">Total:</Form.Label>
-          <br />
-          <Form.Control
-            type="string"
-            placeholder="$7000"
-            name="all"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            className="form-input"
-            required
-          />
-        </Form.Group>
+          <Form.Group controlId="all">
+            <Form.Label className="form-label">Total:</Form.Label>
+            <br />
+            <Form.Control
+              type="string"
+              placeholder="$7000"
+              name="all"
+              value={formData.all}
+              onChange={handleChange}
+              className="form-input"
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId="gps">
+          <Form.Group controlId="gps">
+            <Form.Label className="form-label">Ubicación exacta</Form.Label>
+            <br />
+            <Form.Control
+              type="text"
+              placeholder='La Estación Rincón, Avenida Universidad 1001, El Potrero, 20400 Rincón de Romos, Ags.'
+              name="gps"
+              value={formData.gps}
+              onChange={handleChange}
+              className="form-input"
+              required
+            />
+          </Form.Group>
+        </Form>
         <br />
-       
-          <Form.Label className="form-label">Ubicación exacta</Form.Label>
-          <br />
-          <Form.Control
-           type="text"
-            name="gps"
-            value={formData.municipality}
-            onChange={handleChange}
-            className="form-input"
-            required
-          >
-           
-         
-            {/* Agregar más municipios de Aguascalientes según sea necesario */}
-          </Form.Control>
-       
-        </Form.Group>
-     
-       
-      </Form>
-      <br />
-     
-      <Button variant="primary" type="submit" className="submit-button">
+        <Button variant="primary" type="submit" className="submit-button">
           Registrar cita
         </Button>
 
         <div className="footer">
-      <img className ="logoo" src={logoo} alt="Logo" />
-      <img src={utna} alt="Logo" className="utna" />
-  <img src={utma} alt="Logo" className="utma" />
-  <img src={z3} alt="Logo" className="z3" />
-  <img src={icphub} alt="Logo" className="icph" />
-  <img src={df} alt="Logo" className="df" />
-
-    </div>
-    </Container>
- 
+          <img className="logoo" src={logoo} alt="Logo" />
+          <img src={utna} alt="Logo" className="utna" />
+          <img src={utma} alt="Logo" className="utma" />
+          <img src={z3} alt="Logo" className="z3" />
+          <img src={icphub} alt="Logo" className="icph" />
+          <img src={df} alt="Logo" className="df" />
+        </div>
+      </Container>
     </body>
-
   );
 };
 
--- src/frontend/src/components/registro.jsx
+++ src/frontend/src/components/registro.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
+import Citas from './citas'; // Asegúrate de que la ruta sea correcta
 import '../components/registro.css'; // Importa el archivo de estilos CSS
-import logo from '../../public/log.png';
-import logoo from '../../public/ic.png';
-import nFondo from '../../public/logoo.png';
-import utna from '../../public/utnaa.png';
-import utma from '../../public/utmaa.png';
-import icphub from '../../public/icphub.png';
-import z3 from '../../public/z3.jpeg';
-import df from '../../public/df.png';
+import logo from '/log.png';
+import logoo from '/ic.png';
+import utna from '/utnaa.png';
+import utma from '/utmaa.png';
+import icphub from '/icphub.png';
+import z3 from '/z3.jpeg';
+import df from '/df.png';
 
 const RegisterForm = () => {
   const [formData, setFormData] = useState({
@@ -18,7 +18,7 @@ const RegisterForm = () => {
     municipality: 'Seleccione un municipio',
   });
 
-  const [formSubmitted, setFormSubmitted] = useState(false); // Estado para controlar si el formulario ha sido enviado
+  const [redirectToCitas, setRedirectToCitas] = useState(false); // Estado para redirigir
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,121 +27,111 @@ const RegisterForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Verificar si todos los campos requeridos están completos
     if (formData.companyName && formData.email && formData.phoneNumber && formData.municipality !== 'Seleccione un municipio') {
-      // Aquí puedes realizar acciones como enviar los datos a una API
       console.log(formData);
-      // Marcar el formulario como enviado
-      setFormSubmitted(true);
+      setRedirectToCitas(true); // Establecer redirección
     } else {
       alert('Por favor complete todos los campos antes de enviar el formulario.');
     }
   };
 
-  // Redirigir si el formulario ha sido enviado correctamente
-  if (formSubmitted) {
-    return <Redirect to="" />; // Reemplaza "/ruta-de-tu-pagina" con la ruta real a donde deseas redirigir
+  const handleButtonClick = () => {
+    // Crear un objeto de evento simulado
+    const simulatedEvent = {
+      preventDefault: () => {},
+    };
+
+    // Llamar al handleSubmit con el evento simulado
+    handleSubmit(simulatedEvent);
+  };
+
+  if (redirectToCitas) {
+    return <Citas />; // Renderizar el componente de destino
   }
 
   return (
-    <body>  
-    <div className="body-container" style={{height: '50px'}}>
-      <img src={logo} alt="Logo" className="logo" />
+    <div>
+      <div className="body-container" style={{ height: '50px' }}>
+        <img src={logo} alt="Logo" className="logo" />
+       
       </div>
-    <Container className="register-form-container">
-      <h2 className="form-title">Registro de Empresa</h2>
-      <Form onSubmit={handleSubmit} className="form">
-        <Form.Group controlId="companyName">
-        <br />
-      
-          <Form.Label className="form-label">Nombre de la Empresa</Form.Label>
-          <br />
-          <Form.Control
-            type="text"
-            placeholder="Ingrese el nombre de la empresa"
-            name="companyName"
-            value={formData.companyName}
-            onChange={handleChange}
-            className="form-input"
-            required
-          />
+      <Container className="register-form-container">
+        <h2 className="form-title">Registro de Empresa</h2>
+        <Form className="form">
+          <Form.Group controlId="companyName">
+         
+            <Form.Label className="form-label">Nombre de la Empresa</Form.Label>
             <br />
-           
-        </Form.Group>
-      
-        <Form.Group controlId="email">
-          <Form.Label className="form-label">Email</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese el nombre de la empresa"
+              name="companyName"
+              value={formData.companyName}
+              onChange={handleChange}
+              className="form-input"
+              required
+            />
+            <br />
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label className="form-label">Email</Form.Label>
+            <br />
+            <Form.Control
+              type="email"
+              placeholder="Ingrese el email de la empresa"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="form-input"
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="phoneNumber">
+            <Form.Label className="form-label">Teléfono</Form.Label>
+            <br />
+            <Form.Control
+              type="tel"
+              placeholder="Ingrese el teléfono de la empresa"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              className="form-input"
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="municipality">
+            <Form.Label className="form-label">Municipio</Form.Label>
+            <br />
+            <Form.Control
+              as="select"
+              name="municipality"
+              value={formData.municipality}
+              onChange={handleChange}
+              className="form-input"
+              required
+            >
+              <option disabled>Seleccione un municipio</option>
+              <option>Aguascalientes</option>
+              <option>Asientos</option>
+              <option>Calvillo</option>
+              {/* Agregar más municipios de Aguascalientes según sea necesario */}
+            </Form.Control>
+          </Form.Group>
           <br />
-          <Form.Control
-            type="email"
-            placeholder="Ingrese el email de la empresa"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="form-input"
-            required
-          />
-        </Form.Group>
-
-        <Form.Group controlId="phoneNumber">
-             <br />
-          <Form.Label className="form-label">Teléfono</Form.Label>
-          <br />
-          <Form.Control
-            type="tel"
-            placeholder="Ingrese el teléfono de la empresa"
-            name="phoneNumber"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            className="form-input"
-            required
-          />
-        </Form.Group>
-
-        <Form.Group controlId="municipality">
-        <br />
-       
-          <Form.Label className="form-label">Municipio</Form.Label>
-          <br />
-          <Form.Control
-            as="select"
-            name="municipality"
-            value={formData.municipality}
-            onChange={handleChange}
-            className="form-input"
-            required
-          >
-           
-            <option disabled>Seleccione un municipio</option>
-            <option>Aguascalientes</option>
-            <option>Asientos</option>
-            <option>Calvillo</option>
-            {/* Agregar más municipios de Aguascalientes según sea necesario */}
-          </Form.Control>
-       
-        </Form.Group>
-     
-       
-      </Form>
-      <br />
-     
-      <Button variant="primary" type="submit" className="submit-button">
+        </Form>
+        <Button variant="primary" className="submit-button" onClick={handleButtonClick}>
           Registrar
         </Button>
-
         <div className="footer">
-      <img className ="logoo" src={logoo} alt="Logo" />
-      <img src={utna} alt="Logo" className="utna" />
-  <img src={utma} alt="Logo" className="utma" />
-  <img src={z3} alt="Logo" className="z3" />
-  <img src={icphub} alt="Logo" className="icph" />
-  <img src={df} alt="Logo" className="df" />
-
+          <img className="logoo" src={logoo} alt="Logo" />
+          <img src={utna} alt="Logo" className="utna" />
+          <img src={utma} alt="Logo" className="utma" />
+          <img src={z3} alt="Logo" className="z3" />
+          <img src={icphub} alt="Logo" className="icph" />
+          <img src={df} alt="Logo" className="df" />
+        </div>
+      </Container>
     </div>
-    </Container>
- 
-    </body>
-
   );
 };
 
